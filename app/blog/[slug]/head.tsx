054388f5f0896fc516ee86fee529ead55d433eeb
murdersnakes <92579054+murdersnakes@users.noreply.github.com>
2023-02-21 15:34:47 +0000

    init 5

--- app/blog/[slug]/head.tsx
+++ app/blog/[slug]/head.tsx
@@ -1,39 +1,38 @@
 import React from "react";
+import posts from "../../../data/posts";
+import { default as slugify } from "slugify";
 
-export default function head() {
+export default function BlogPostHead({ params }) {
+  let post = posts.find(
+    (x): boolean =>
+      params.slug ==
+      slugify(x.title, { remove: /[*+~.()'"!:@]/g }).toLocaleLowerCase()
+  );
+
   return (
     <>
       <meta charSet='utf-8' />
       <meta name='language' content='english' />
       <meta http-equiv='content-type' content='text/html' />
-      <meta name='author' content="Michael O'Reilly" />
+      <meta name='author' content={post.author} />
       <meta name='designer' content="Michael O'Reilly" />
       <meta name='publisher' content="Michael O'Reilly" />
-      <title>SaltyDogs | Home</title>
+      <title>{post.title}</title>
       <meta name='robots' content='index,follow' />
       <meta name='revisit-after' content='7 days' />
       <meta name='distribution' content='web' />
       <meta http-equiv='refresh' content='30' />
       <meta name='robots' content='noodp' />
-      <meta
-        name='keywords'
-        content='Travel, Lifestyle, Food, Fashion, Pets, Adventure, Health, Fitness, Culture, Entertainment'
-      />
-      <meta
-        name='description'
-        content='Discover the latest articles on SaltyDogs, a blog covering a wide range of topics. From travel to lifestyle and everything in between, our content is sure to inspire and inform. Visit us today!'
-      />
+      <meta name='keywords' content={post.metaKeywords} />
+      <meta name='description' content={post.metaDescription} />
       <meta name='subject' content='A collection of salty blog posts' />
       <meta name='copyright' content='SaltyDogs' />
       <meta name='language' content='EN' />
-      <meta name='og:title' content='SaltyDogs | Home' />
+      <meta name='og:title' content={post.title} />
       <meta name='og:type' content='article' />
-      <meta name='og:image' content='/salty_dog.webp' />
+      <meta name='og:image' content={post.image} />
       <meta name='og:site_name' content='SaltyDogs' />
-      <meta
-        name='og:description'
-        content='Discover the latest articles on SaltyDogs, a blog covering a wide range of topics. From travel to lifestyle and everything in between, our content is sure to inspire and inform. Visit us today!'
-      />
+      <meta name='og:description' content={post.title} />
       <meta name='format-detection' content='telephone=yes' />
       <meta name='HandheldFriendly' content='true' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
